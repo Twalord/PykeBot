48d328e6a5cbde81bf3ceb7fb604b77dba541413
jonathan.decker <jonathan.decker@stud.uni-goettingen.de>
2019-02-20 21:44:17 +0100

    added debug_tools to help with testing and debugging, see module description for usage

---
+++ debug_tools.py
@@ -0,0 +1,45 @@
+import logging
+import functools
+import time
+
+logger = logging.getLogger('scrap_logger')
+
+"""
+This decorators can be used to debug functions.
+In order to use them import this module and add @functionname
+on top of the function def you wish to debug.
+For example:
+
+@timer
+def do_something(value):
+    # do work
+    return
+
+"""
+
+
+def timer(func):
+    # log the runtime of the decorated function
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        logger.debug(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+    return wrapper_timer
+
+
+def debug(func):
+    # log the function signature and return value
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        logger.debug(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        logger.debug(f"{func.__name__!r} returned {value!r}")
+        return value
+    return wrapper_debug
