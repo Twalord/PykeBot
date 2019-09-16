d1cc9a7746e48bef4d437c3c916496839d379aee
jonathan.decker <jonathan.decker@stud.uni-goettingen.de>
2019-09-16 15:59:45 +0200

    Added documentation to premiertour stalker refactoring

--- stalker/premiertour_stalker.py
+++ stalker/premiertour_stalker.py
@@ -15,6 +15,11 @@ logger = logging.getLogger('scrap_logger')
 
 
 def stalk(premiertour_link):
+    """
+    Stalks all teams signed up for the given premiertour tournament, only uses requests
+    :param premiertour_link: String, valid url to the main page of the premiertour tournament
+    :return: TeamList, containing the Team obj for each signed uo team
+    """
 
     logger.debug("Beginning stalk for " + premiertour_link)
 
@@ -29,16 +34,7 @@ def stalk(premiertour_link):
     # find all teams and save the links
     # find first div class=list-section to only consider valid participants
     # grab div class=section-content from it and extract all hrefs
-    team_container = create_team_container(edited_premiertour_link)
-    links = []
-    for container in team_container:
-        # checks if the second table with incomplete sign ups has been reached
-        table_check = container.find("span", class_="table-cell-item txt-status-pending")
-        if table_check:
-            if "Nicht genug Spieler" in table_check.text:
-                continue
-        links.extend([link["href"] for link in container.find_all("a", href=True)])
-    all_links = list(dict.fromkeys(links))
+    all_links = grab_team_links(edited_premiertour_link)
 
     # open each link, collect summoner names
     single_tasks = []
@@ -52,6 +48,11 @@ def stalk(premiertour_link):
 
 
 def stalk_team(url):
+    """
+    Takes the url to a premiertour team page and returns a Team obj for the players of the team
+    :param url: String, valid link to a premiertour team page
+    :return: Team, containing the players of the team
+    """
     # request page information
     page = requests.get(url)
     soup = bs4.BeautifulSoup(page.text, features="html.parser")
@@ -85,6 +86,11 @@ def stalk_team(url):
 
 
 def get_title(url):
+    """
+    Takes the link to the premiertour main page and returns the title of the tournament
+    :param url: String, valid link to the main page of a premiertour tournament
+    :return: String, the title of the tournament
+    """
 
     main_page = requests.get(url)
     main_premiertour_soup = bs4.BeautifulSoup(main_page.text, features="html.parser")
@@ -92,13 +98,23 @@ def get_title(url):
     return tournament_name
 
 
-def create_team_container(url):
-
+def grab_team_links(url):
+    """
+    Takes the url to the participants page of a premiertour tournament and returns all links to valid teams
+    :param url: String, valid link to the participants page of a premiertour tournament
+    :return: List(String), list of urls to all teams with valid sign ups
+    """
     page = requests.get(url)
     html_string = page.text
     premiertour_soup = bs4.BeautifulSoup(html_string, features="html.parser")
-    return premiertour_soup.find_all("tr")
-    # team_container = premiertour_soup.find('section', class_="list-section")
-    # team_container = team_container.find('table', class_="table table-fixed-single table-responsive")
+    team_container = premiertour_soup.find_all("tr")
 
-    # return team_container
+    links = []
+    for container in team_container:
+        # checks if the second table with incomplete sign ups has been reached
+        table_check = container.find("span", class_="table-cell-item txt-status-pending")
+        if table_check:
+            if "Nicht genug Spieler" in table_check.text:
+                continue
+        links.extend([link["href"] for link in container.find_all("a", href=True)])
+    return list(dict.fromkeys(links))
